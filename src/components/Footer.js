1cc250822f7a31f0d6fb16617d5ac09d608fd9df
Ankit Hans <ankithans1947@gmail.com>
2021-02-24 01:29:37 +0530

    fix footer font

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ export default function footer() {
   return (
     <div>
       <div className="container px-5 py-6 mx-auto flex items-center  flex-col">
-        <p className="text-sm text-gray-500  sm:ml-6 sm:mt-0 mt-4">
+        <p className="text-sm text-gray-500 body-font  sm:ml-6 sm:mt-0 mt-4">
           © 2021 Ankit Hans —
           <a
             href="https://twitter.com/AnkitHans15"
