e22d45f1870bab75f78c1967f4a1723265f6e36b
Ankit Hans <ankithans1947@gmail.com>
2021-03-03 22:33:58 +0530

    chore: update resume link

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,7 +4,7 @@ export default function Resume() {
   return (
     <a
       target="_blank"
-      href="https://drive.google.com/file/d/1a8lgm9JE_6tGkhT60PE2KDbNkTJK-ZxI/view?usp=sharing"
+      href="https://drive.google.com/file/d/1PxQ4BUzlnTty6s7OwIFJB1zbxGqXekFO/view?usp=sharing"
       className="float-right pt-11 lg:pt-0 pb-20 pl-20 body-font text-gray-400 font-medium text-lg"
     >
       Resume
